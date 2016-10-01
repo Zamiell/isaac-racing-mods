b3abcfe1ee6e0a4a7ddbe2318eaca9e26c917abd
James <j.nesta@gmail.com>
2016-10-01 19:16:37 -0400

    oops

--- program/languages.py
+++ program/languages.py
@@ -4,175 +4,215 @@ text = {
     'Select a mod': {
         'en': 'Select a mod',
         'fr': 'Sélectionnez un mod',
+        'es': '',
     },
     'Miscellaneous Stuff': {
         'en': 'Miscellaneous Stuff',
         'fr': 'Divers',
+        'es': '',
     },
     'Exit': {
         'en': 'Exit',
         'fr': 'Quitter',
+        'es': '',
     },
 
     # Jud6s
     'Select a ruleset': {
         'en': 'Select a ruleset',
         'fr': 'Sélectionnez une option',
+        'es': '',
     },
 
     # Instant Start
     'Choose a Start': {
         'en': 'Choose a Start',
         'fr': 'Choisissez un Starter',
+        'es': '',
     },
     'Random Start': {
         'en': 'Random Start',
         'fr': 'Starter Aléatoire',
+        'es': '',
     },
     'Lost Child Open': {
         'en': 'Lost Child Open',
         'fr': 'Tournoi The Lost Child',
+        'es': '',
     },
     'Click a build to play it': {
         'en': 'Click a build to play it',
         'fr': 'Cliquez sur une option pour la jouer',
+        'es': '',
     },
     'Search': {
         'en': 'Search',
         'fr': 'Rechercher',
+        'es': '',
     },
     'Treasure Room Starts': {
         'en': 'Treasure Room Starts',
         'fr': 'Starters Treasure Room',
+        'es': '',
     },
     'Devil Room Starts': {
         'en': 'Devil Room Starts',
         'fr': 'Starters Devil Room',
+        'es': '',
     },
     'Angel Room Starts': {
         'en': 'Angel Room Starts',
         'fr': 'Starters Angel Room',
+        'es': '',
     },
     'Custom Starts (with the D6)': {
         'en': 'Custom Starts (with the D6)',
         'fr': 'Starters Spéciaux (avec D6)',
+        'es': '',
     },
     'Custom Starts (without the D6)': {
         'en': 'Custom Starts (without the D6)',
         'fr': 'Starters Spéciaux (sans D6)',
+        'es': '',
     },
 
     # Diversity Mod
     'Enter a seed': {
         'en': 'Enter a seed',
         'fr': 'Entrez une seed',
+        'es': '',
     },
     'Start Diversity Mod': {
         'en': 'Start Diversity Mod',
         'fr': 'Commencer le Mod Diversity',
+        'es': '',
     },
 
     # Miscellaneous Stuff
     'Select an option': {
         'en': 'Select an option',
         'fr': 'Sélectionnnez une option',
+        'es': '',
     },
     'Remove boss cutscenes': {
         'en': 'Remove boss cutscenes',
         'fr': 'Enlever les cinématiques des boss',
+        'es': '',
     },
     'Automatically attempt to close Isaac': {
         'en': 'Automatically attempt to close Isaac',
         'fr': 'Fermeture automatique d\'Isaac',
+        'es': '',
     },
     'Open Isaac game directory': {
         'en': 'Open Isaac game directory',
         'fr': 'Ouvrir le répertoire du jeu Isaac',
+        'es': '',
     },
     'Open Isaac documents directory': {
         'en': 'Open Isaac documents directory',
         'fr': 'Ouvrir le répertoire de documents d\'Isaac',
+        'es': '',
     },
     'Uninstall all existing mods': {
         'en': 'Uninstall all existing mods',
         'fr': 'Désinstaller tous les mods',
+        'es': '',
     },
     'Visit the website for': {
         'en': 'Visit the website for',
         'fr': 'Visiter le site',
+        'es': '',
     },
 
     # Miscellaneous
     'Isaac will open': {
         'en': 'Isaac will open when you start the mod.',
         'fr': 'Isaac va s\'ouvrir automatiquement au lancement d\'un mod.',
+        'es': '',
     },
     'Keep this program open': {
         'en': 'Keep this program open while playing.',
         'fr': 'Laissez ce programme ouvert pendant que vous jouez.',
+        'es': '',
     },
     'Isaac will return to normal': {
         'en': 'Isaac will return to normal when this program is closed.',
         'fr': 'Isaac retournera à sa configuration d\'origine à la fermeture de ce programme.',
+        'es': '',
     },
     'About these options': {
         'en': 'About these options',
         'fr': 'A propos des options',
+        'es': '',
     },
     'About this mod': {
         'en': 'About this mod',
         'fr': 'A propos du mod',
+        'es': '',
     },
     'Go Back': {
         'en': 'Go Back',
         'fr': 'Retour',
+        'es': '',
     },
 
     # Errors and warnings
     'Error': {
         'en': 'Error',
         'fr': 'Erreur',
+        'es': '',
     },
     'Warning': {
         'en': 'Warning',
         'fr': 'Attention',
+        'es': '',
     },
     'Generic error': {
         'en': 'Generic error',
         'fr': 'Erreur générique',
+        'es': '',
     },
     'was unable to find your Isaac resources directory.': {
         'en': 'was unable to find your Isaac resources directory.',
         'fr': 'n\'a pas pu trouver le répertoire des ressources d\'Isaac.',
+        'es': '',
     },
     'Navigate to the program "isaac-ng.exe" in your Steam directory.': {
         'en': 'Navigate to the program "isaac-ng.exe" in your Steam directory.',
         'fr': 'Naviguez vers le programme "isaac-ng.exe" dans votre répertoire Steam.',
+        'es': '',
     },
     'Navigate to "isaac-ng.exe"': {
         'en': 'Navigate to "isaac-ng.exe"',
         'fr': 'Naviguer vers "isaac-ng.exe"',
+        'es': '',
     },
     'Example location': {
         'en': 'Example location',
         'fr': 'Exemple de location',
+        'es': '',
     },
     'The file you selected is not called "isaac-ng.exe".': {
         'en': 'The file you selected is not called "isaac-ng.exe".',
         'fr': 'Le fichier que vous avez sélectionné ne se nomme pas "Isaac-ng.exe".',
+        'es': '',
     },
     'Success': {
         'en': 'Success',
         'fr': 'Succés',
+        'es': '',
     },
     'You have successfully set': {
         'en': 'You have successfully set your Isaac resources directory. Click OK to restart ',
         'fr': 'Vous avez réussi à rediriger le programme vers le répertoire de ressources d\'Isaac. Cliquez sur OK pour redémarrer l\'',
+        'es': '',
     },
 
     # Template
     '': {
         'en': '',
         'fr': '',
+        'es': '',
     },
 }
